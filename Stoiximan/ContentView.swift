b1ea2ad25c4fcabc0edf42a4c43f387a3593aa70
Vasilis Zotikas <vzotikas@gmail.com>
2022-02-12 13:53:03 +0100

    Initial Commit

---
+++ Stoiximan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stoiximan
+//
+//  Created by vasse on 2/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stoiximan/StoiximanApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoiximanApp.swift
+//  Stoiximan
+//
+//  Created by vasse on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StoiximanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
